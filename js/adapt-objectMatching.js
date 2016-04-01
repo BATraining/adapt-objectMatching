9585536af7483331356aa6a4b528c712a0e33872
Himanshu Rajotia <hrajotia@gmail.com>
2016-04-01 16:04:56 +0530

    minor refactor

--- js/adapt-objectMatching.js
+++ js/adapt-objectMatching.js
@@ -24,8 +24,8 @@ define(function(require) {
     var ObjectMatching = QuestionView.extend({
 
         events: {
-            'click .objectMatching-item-draggable': 'onClickDragItem',
-            'click .objectMatching-item-droppable': 'onClickDropItem'
+            'click .draggable-item': 'onClickDragItem',
+            'click .droppable-item': 'onClickDropItem'
         },
 
         resetQuestionOnRevisit: function() {
@@ -36,18 +36,21 @@ define(function(require) {
             this.listenTo(Adapt, 'device:changed', this.reRender, this);
             this.model.set('_currentlySelectedItemId', '');
 
-            // Check if items need to be randomised
-            if (this.model.get('_isRandom') && this.model.get('_isEnabled')) {
-                this.model.set('_draggableItems', _.shuffle(this.model.get('_draggableItems')));
-            }
-
-            if(!this.model.get('_isSubmitted')) {
-                this.model.set('_droppableItems', _.shuffle(this.model.get('_droppableItems')));
-            }
+            this.setupRandomisation();
 
             _.each(this.model.get("_draggableItems"), function(item, index) {
                 item._index = item._index || (index + 1);
             });
+        },
+
+        setupRandomisation: function() {
+            if (this.model.get('_isRandom') && this.model.get('_isEnabled')) {
+                this.model.set('_draggableItems', _.shuffle(this.model.get('_draggableItems')));
+            }
+
+            if (!this.model.get('_isSubmitted')) {
+                this.model.set('_droppableItems', _.shuffle(this.model.get('_droppableItems')));
+            }
         },
 
         onQuestionRendered: function() {
@@ -64,7 +67,7 @@ define(function(require) {
 
         setupObjectMatching: function() {
             var draggableItems = this.model.get('_draggableItems');
-            _.each(this.$('.objectMatching-item-draggable-index'), function(item, index) {
+            _.each(this.$('.draggable-item-index'), function(item, index) {
                 $(item).html(draggableItems[index]._index);
             });
 
@@ -81,17 +84,20 @@ define(function(require) {
 
         replaceWithDragAndDrop: function() {
             if (!Adapt.componentStore.dragAndDrop) throw "Drag And Drop not included in build";
-            var DragAndDrop = Adapt.componentStore.dragAndDrop;
 
+            var DragAndDrop = Adapt.componentStore.dragAndDrop;
             var model = this.prepareDragAndDropModel();
-            var newDragAndDrop = new DragAndDrop({model: model, $parent: this.options.$parent});
+            var newDragAndDrop = new DragAndDrop({model: model});
+            var $container = $(".component-container", $("." + this.model.get("_parentId")));
+
             if(model.get('_initialDragAndDropWidgetView')) {
                 newDragAndDrop.$el.find('.dragAndDrop-widget').html(model.get('_initialDragAndDropWidgetView'));
             }
             if(model.get('_isSubmitted')) {
                 newDragAndDrop.showMarking();
             }
-            this.options.$parent.append(newDragAndDrop.$el);
+
+            $container.append(newDragAndDrop.$el);
             this.remove();
             _.defer(function() {
                 Adapt.trigger('device:resize');
@@ -101,7 +107,6 @@ define(function(require) {
         prepareDragAndDropModel: function() {
             var model = this.model;
             model.set('_component', 'dragAndDrop');
-
             return model;
         },
 
@@ -113,20 +118,20 @@ define(function(require) {
             }
 
             var $selectedElement = this.$(event.currentTarget);
-            var $selectedElementContainer = $selectedElement.parent('div');
+            var $selectedElementContainer = $selectedElement.closest('.objectMatching-item-wrapper');
             var currentlySelectedItemId = $selectedElement.attr('data-id');
             var currentlySelectedItem = _.where(this.model.get('_draggableItems'), {id: currentlySelectedItemId})[0];
+
             if (currentlySelectedItemId == this.model.get('_currentlySelectedItemId')) {
-                $selectedElementContainer
-                    .removeClass('selected');
-                this.$('.objectMatching-item-droppable').removeClass('cursor-pointer');
+                $selectedElementContainer.removeClass('selected');
+                this.$('.droppable-item').removeClass('cursor-pointer');
                 this.model.set('_currentlySelectedItemId', '');
             } else {
                 $selectedElementContainer
                     .addClass('selected')
                     .siblings('div')
                     .removeClass('selected');
-                this.$('.objectMatching-item-droppable').addClass('cursor-pointer');
+                this.$('.droppable-item').addClass('cursor-pointer');
                 this.model.set('_currentlySelectedItemId', currentlySelectedItem.id);
             }
         },
@@ -137,16 +142,17 @@ define(function(require) {
             var currentlySelectedItemId = this.model.get('_currentlySelectedItemId');
             if (!this.model.get('_isEnabled') || !currentlySelectedItemId) return false;
 
-            var $selectedElementContainer = this.$(event.currentTarget).closest('.objectMatching-item-droppable');
+            var $selectedElement = this.$(event.currentTarget);
+            var $selectedElementContainer = this.$(event.currentTarget).closest('.objectMatching-item-wrapper');
 
-            var selectedElementId = $selectedElementContainer.attr('data-id');
+            var selectedElementId = $selectedElement.attr('data-id');
 
             var draggableItems = this.model.get('_draggableItems');
             var droppableItems = this.model.get('_droppableItems');
             var selectedItem = _.where(droppableItems, {id: selectedElementId})[0];
 
             if(selectedItem._selectedItemId && selectedItem._selectedItemId == currentlySelectedItemId) {
-                $selectedElementContainer.find('.objectMatching-item-droppable-index').html('');
+                $selectedElementContainer.find('.droppable-item-index').html('');
                 $selectedElementContainer.removeClass("selected");
                 selectedItem._selectedItemId = null;
             } else {
@@ -154,7 +160,7 @@ define(function(require) {
 
                 if(existingSelectedItem) {
                     this.$('[data-id=' + existingSelectedItem.id + ']')
-                        .find('.objectMatching-item-droppable-index').html('');
+                        .find('.droppable-item-index').html('');
                     $selectedElementContainer.removeClass("selected");
                     existingSelectedItem._selectedItemId = null;
                 }
@@ -163,7 +169,7 @@ define(function(require) {
                     .siblings('div')
                     .removeClass('selected');
                 $selectedElementContainer
-                    .find('.objectMatching-item-droppable-index')
+                    .find('.droppable-item-index')
                     .html(_.where(draggableItems, {id: currentlySelectedItemId})[0]._index);
 
                 selectedItem._selectedItemId = currentlySelectedItemId;
@@ -182,8 +188,8 @@ define(function(require) {
             var canSubmit = (count == this.model.get('_droppableItems').length);
 
             if(canSubmit) {
-                this.$('.objectMatching-item-draggable-wrapper').removeClass('selected');
-                this.$('.objectMatching-item-droppable').removeClass("selected");
+                this.$('.objectMatching-item-wrapper').removeClass('selected');
+                this.$('.droppable-item').removeClass("selected");
             }
 
             return canSubmit;
@@ -239,7 +245,7 @@ define(function(require) {
         showMarking: function() {
             _.each(this.model.get('_droppableItems'), function(item, i) {
 
-                var $item = this.$('.objectMatching-item-droppable').eq(i);
+                var $item = this.$('.droppable-item-wrapper').eq(i);
                 $item.addClass(item._isCorrect ? 'correct' : 'incorrect');
 
             }, this);
@@ -274,13 +280,13 @@ define(function(require) {
 
         setdroppableItems: function(droppableContainerIndex, draggableItemId) {
 
-            var $droppableItemContainer = this.$('.objectMatching-item-droppable').eq(droppableContainerIndex);
+            var $droppableItemContainer = this.$('.droppable-item').eq(droppableContainerIndex);
             var draggableItem = _.where(this.model.get('_draggableItems'), {id: draggableItemId})[0];
 
             $droppableItemContainer
 
-                .find('.objectMatching-item-droppable-index')
-                .html(this.$('.objectMatching-item-draggable').index(this.$('[data-id=' + draggableItem.id + ']')) + 1);
+                .find('.droppable-item-index')
+                .html(this.$('.draggable-item').index(this.$('[data-id=' + draggableItem.id + ']')) + 1);
         },
 
         hideCorrectAnswer: function() {
